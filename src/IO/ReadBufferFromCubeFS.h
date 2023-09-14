06af434b5e7a4bbe618b9797b2ae430121882e8b
xieyichen <xieyichen@oppo.com>
2023-09-14 10:50:30 +0800

    fix bug

--- src/IO/ReadBufferFromCubeFS.h
+++ src/IO/ReadBufferFromCubeFS.h
@@ -2,18 +2,23 @@
 
 #include <IO/ReadBufferFromFileBase.h>
 
-
-namespace Poco
-{
-class Logger;
-}
 namespace DB
 {
+namespace ErrorCodes
+{
+    extern const int CANNOT_READ_FROM_FILE_DESCRIPTOR;
+    extern const int ARGUMENT_OUT_OF_BOUND;
+    extern const int CANNOT_SEEK_THROUGH_FILE;
+    extern const int CANNOT_SELECT;
+    extern const int CANNOT_FSTAT;
+    extern const int CANNOT_ADVISE;
+}
 class ReadBufferFromCubeFS : public ReadBufferFromFileBase
 {
 public:
     explicit ReadBufferFromCubeFS(
-        int64_t id_ const std::string & file_name_,
+        int64_t id_,
+        const std::string & file_name_,
         size_t buf_size = DBMS_DEFAULT_BUFFER_SIZE,
         int flags = -1,
         char * existing_memory = nullptr,
@@ -23,6 +28,7 @@ public:
     std::string getFileName() const override { return file_name; }
     Range getRemainingReadRange() const override { return Range{.left = file_offset_of_buffer_end, .right = std::nullopt}; }
     size_t getFileOffsetOfBufferEnd() const override { return file_offset_of_buffer_end; }
+    off_t getPosition() override { return file_offset_of_buffer_end - (working_buffer.end() - pos); }
     void close();
     /** Could be used before initialization if needed 'fd' was not passed to constructor.
       * It's not possible to change 'fd' during work.
@@ -30,13 +36,12 @@ public:
     void setFD(int fd_) { fd = fd_; }
     int getFD() const { return fd; }
     off_t size() const;
-    Poco::Logger * log = &Poco::Logger::get("WriteBufferFromCubeFS");
-
 
 private:
-    void nextImpl() override;
+    bool nextImpl() override;
     int fd;
     int64_t id;
     std::string file_name;
+    size_t file_offset_of_buffer_end = 0; /// What offset in file corresponds to working_buffer.end().
 };
 }
