73c32b2bcda0f0201b94256f3506b4d9f4ab879d
xieyichen <xieyichen@oppo.com>
2023-10-08 16:14:50 +0800

    spilt unit test

--- src/Disks/tests/gtest_disk_cubefs.cpp
+++ src/Disks/tests/gtest_disk_cubefs.cpp
@@ -1,3 +1,4 @@
+#include <string>
 #include <gtest/gtest.h>
 #define RUN_CUBEFS_TEST 1
 #if RUN_CUBEFS_TEST
@@ -21,69 +22,107 @@ const String push_addr = "cfs.dg-push.wanyol.com";
 const String disk_name = "cubefs";
 
 
-TEST(DiskTestCubeFS, CreateDirectories)
+class DiskTestCubeFS : public testing::Test
 {
-    auto id = DB::getClientId();
-    std::cout << "get id: " << id << std::endl;
-    auto settings
-        = std::make_unique<DB::DiskCubeFSSettings>(id, vol_name, master_addr, log_dir, log_level, access_key, secret_key, push_addr);
+public:
+    void SetUp() override
+    {
+        auto id = DB::getClientId();
+        std::cout << "get id: " << id << std::endl;
+        auto settings
+            = std::make_unique<DB::DiskCubeFSSettings>(id, vol_name, master_addr, log_dir, log_level, access_key, secret_key, push_addr);
 
-    DB::setClientInfo(settings->id, "volName", const_cast<char *>(settings->vol_name.data()));
-    DB::setClientInfo(settings->id, "masterAddr", const_cast<char *>(settings->master_addr.data()));
-    DB::setClientInfo(settings->id, "logDir", const_cast<char *>(settings->log_dir.data()));
-    std::cout << "log dir: " << const_cast<char *>(settings->log_dir.data()) << std::endl;
-    DB::setClientInfo(settings->id, "logLevel", const_cast<char *>(settings->log_level.data()));
-    DB::setClientInfo(settings->id, "accessKey", const_cast<char *>(settings->access_key.data()));
-    DB::setClientInfo(settings->id, "secretKey", const_cast<char *>(settings->secret_key.data()));
-    DB::setClientInfo(settings->id, "pushAddr", const_cast<char *>(settings->push_addr.data()));
+        DB::setClientInfo(settings->id, "volName", const_cast<char *>(settings->vol_name.data()));
+        DB::setClientInfo(settings->id, "masterAddr", const_cast<char *>(settings->master_addr.data()));
+        DB::setClientInfo(settings->id, "logDir", const_cast<char *>(settings->log_dir.data()));
+        std::cout << "log dir: " << const_cast<char *>(settings->log_dir.data()) << std::endl;
+        DB::setClientInfo(settings->id, "logLevel", const_cast<char *>(settings->log_level.data()));
+        DB::setClientInfo(settings->id, "accessKey", const_cast<char *>(settings->access_key.data()));
+        DB::setClientInfo(settings->id, "secretKey", const_cast<char *>(settings->secret_key.data()));
+        DB::setClientInfo(settings->id, "pushAddr", const_cast<char *>(settings->push_addr.data()));
 
-    std::cout << "set client info  successfully " << std::endl;
-    std::shared_ptr<DB::IDisk> cubeFSdisk = std::make_shared<DB::DiskCubeFS>(disk_name, path, nullptr, std::move(settings));
-    std::cout << "initial cubefs disk successfully " << std::endl;
+        std::cout << "set client info  successfully " << std::endl;
+        disk = std::make_shared<DB::DiskCubeFS>(disk_name, path, nullptr, std::move(settings));
+        std::cout << "initial cubefs disk successfully " << std::endl;
+    }
+    void TearDown() override
+    {
+        disk->removeRecursive(disk->getPath());
+        disk.reset();
+    }
 
-    EXPECT_EQ(disk_name, cubeFSdisk->getName());
-    EXPECT_EQ(path, cubeFSdisk->getPath());
+    DB::DiskPtr disk;
+};
+
+TEST(DiskTestCubeFS, CreateDirectory)
+{
+    EXPECT_EQ(disk_name, this->disk->getName());
 
     cubeFSdisk->createDirectory("create_directory");
-    EXPECT_TRUE(cubeFSdisk->exists("create_directory"));
-    EXPECT_TRUE(cubeFSdisk->isDirectory("create_directory"));
-    EXPECT_FALSE(cubeFSdisk->isFile("create_directory"));
+    EXPECT_TRUE(this->disk->exists("create_directory"));
+    EXPECT_TRUE(this->disk->isDirectory("create_directory"));
+    EXPECT_FALSE(this->disk->isFile("create_directory"));
 
-    cubeFSdisk->createDirectories("not_exist_parent_directory/subdirectory");
-    EXPECT_TRUE(cubeFSdisk->isDirectory("not_exist_parent_directory/subdirectory"));
-
-    try
-    {
-        EXPECT_THROW(cubeFSdisk->moveDirectory("not_exist_parent_directory/subdirectory", "not_exist/create_directory/"), std::exception);
-    }
-    catch (const std::exception & e)
-    {
-        EXPECT_EQ(e.code(), ErrorCodes::DIRECTORY_DOESNT_EXIST);
-    }
-
-    cubeFSdisk->moveDirectory("not_exist_parent_directory/subdirectory", "create_directory/");
-    EXPECT_TRUE(cubeFSdisk->isDirectory("create_directory/subdirectory"));
-
-    cubeFSdisk->createFile("create_directory/file");
-    EXPECT_TRUE(cubeFSdisk->isFile("create_directory/file"));
-    EXPECT_FALSE(cubeFSdisk->isDirectory("create_directory/file"));
-
-    cubeFSdisk->moveDirectory("create_directory", "move_directory");
-    EXPECT_TRUE(cubeFSdisk->isFile("move_directory/file"));
-
-    cubeFSdisk->moveFile("move_directory/file", "move_file");
-    EXPECT_TRUE(cubeFSdisk->isFile("move_file"));
-
-    cubeFSdisk->createFile("create_directory/create_file");
-    try
-    {
-        EXPECT_THROW(cubeFSdisk->moveFile("move_file", "create_directory/create_file"), std::exception);
-    }
-    catch (const std::exception & e)
-    {
-        EXPECT_EQ(e.code(), ErrorCodes::FILE_ALREADY_EXISTS);
-    }
+    this->disk->createDirectories("not_exist_parent_directory/subdirectory");
+    EXPECT_TRUE(this->disk->isDirectory("not_exist_parent_directory/subdirectory"));
 }
 
+
+TEST(DiskTestCubeFS, MoveDirectory)
+{
+    try
+    {
+        std::cout << "try move directory" << std::endl;
+        this->disk->moveDirectory("not_exist_parent_directory/subdirectory", "not_exist/create_directory/");
+        FAIL() << "Expected exception to be thrown.";
+        //EXPECT_THROW(cubeFSdisk->moveDirectory("not_exist_parent_directory/subdirectory", "not_exist/create_directory/"), std::exception);
+    }
+    catch (const std::exception & e)
+    {
+//std::cout<<e.what()<<std::endl;
+std::string errorMessage = e.what();
+std::string expectedSubstring = "Destination directory not exist";
+EXPECT_TRUE(errorMessage.find(expectedSubstring) != std::string::npos);
+// EXPECT_THAT(e.what(), testing::HasSubstr("Destination directory not exist"));
+//EXPECT_EQ(e.code(), DB::ErrorCodes::DIRECTORY_DOESNT_EXIST);
+    }
+    std::cout << "try move directory end" << std::endl;
+    this->disk->moveDirectory("not_exist_parent_directory/subdirectory", "create_directory/");
+    EXPECT_TRUE(this->disk->isDirectory("create_directory/subdirectory"));
+    std::cout << "move directory end" << std::endl;
+}
+
+TEST(DiskTestCubeFS, CreateFile)
+{
+    this->disk->createFile("create_directory/file");
+    EXPECT_TRUE(this->disk->isFile("create_directory/file"));
+    EXPECT_FALSE(this->disk->isDirectory("create_directory/file"));
+    std::cout << "create file end" << std::endl;
+    this->disk->moveDirectory("create_directory", "move_directory");
+    EXPECT_TRUE(this->disk->isFile("move_directory/file"));
+    std::cout << "move directory end" << std::endl;
+    this->disk->moveFile("move_directory/file", "move_file");
+    EXPECT_TRUE(this->disk->isFile("move_file"));
+    std::cout << "move file end" << std::endl;
+    this->disk->createFile("create_directory/create_file");
+    std::cout << "create file end" << std::endl;
+    try
+    {
+        std::cout << "try move file" << std::endl;
+        //   EXPECT_THROW(cubeFSdisk->moveFile("move_file", "create_directory/create_file"), std::exception);
+        this->disk->moveFile("move_file", "create_directory/create_file");
+        FAIL() << "Expected exception to be thrown.";
+    }
+    catch (const std::exception & e)
+    {
+        std::string errorMessage = e.what();
+        std::string expectedSubstring = "Destination directory not exist";
+        EXPECT_TRUE(errorMessage.find(expectedSubstring) != std::string::npos);
+
+        //EXPECT_THAT(e.what(), testing::HasSubstr("already exists"));
+        //std::cout<<e.what()<<std::endl;
+        //EXPECT_EQ(e.code(), DB::ErrorCodes::FILE_ALREADY_EXISTS);
+    }
+}
 
 #endif
